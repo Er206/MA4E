e1a69d4ade35309503440430dbc567585eaa8c85
RoxaneKerneuzet <105877123+RoxaneKerneuzet@users.noreply.github.com>
2022-05-19 18:45:17 +0200

    Update solar_farm_agent.py

--- microgrid/agents/solar_farm_agent.py
+++ microgrid/agents/solar_farm_agent.py
@@ -12,14 +12,62 @@ class SolarFarmAgent:
                       previous_state=None,
                       previous_action=None,
                       previous_reward=None):
-        a = self.env.action_space.sample()
-        a = (np.tanh(state['manager_signal'])) / 10
-        if state['soc'] + a[0] * 0.5 > self.env.battery.capacity:
-            a[0] = (self.env.battery.capacity - state['soc']) * 2
-        if state['soc'] + a[0] * 0.5 < 0:
-            a[0] = - state['soc'] * 2
-        return a
+     pb = pulp.LpProblem("ferme_solaire", pulp.LpMinimize)
+        
+        # variables (qu'on cherche à optimiser)
+        l = {} #conso totale
+        a = {} #charge batterie
+        lbat = {} #conso batterie
+        lbatpos = {} #conso batterie partie positive (ça sert pour la valeur absolue)
+        lbatneg = {} #conso batterie négative
+        signelbat = {} #signe de la conso
+        
+        
+        # constantes
+        dt = datetime.timedelta(minutes=30)
+        T = self.env.nb_pdt #on va le chercher dans l'environnement
+        
+        
+        #on va chercher les differentes valeurs des variables dans l'environnement
+        
+        rhoc = self.env.battery.efficiency #efficacité charge 
+        rhod = self.env.battery.efficiency #eff décharge
+        rhomax = self.env.battery.capacity #charge max batterie
+        pmax = self.env.battery.pmax #puissance max
+        
+        surface = self.env.pv.surface
 
+        lPV = 0.001 * surface * state['pv_prevision'] #prod parc pv
+        a[0] = state['soc'] #charge batterie au début
+        prix = state['manager_signal'] #lambda du tp
+
+        #on énonce les variables
+
+        for t in range(T):
+            l[t] = pulp.LpVariable("l_" + str(t), None, None)
+            lbat[t] = pulp.LpVariable("lbat_" + str(t), -pmax, pmax)
+            lbatpos[t] = pulp.LpVariable("lbatpos_" + str(t), 0, pmax)
+            lbatneg[t] = pulp.LpVariable("lbatneg_" + str(t), 0, pmax)
+            signelbat[t] = pulp.LpVariable("signelbat_" + str(t), cat=pulp.LpBinary)
+            a[t + 1] = pulp.LpVariable("a_" + str(t), 0, rhomax)
+
+            #les contraintes
+            pb += lbat[t] == lbatpos[t] - lbatneg[t], 
+            pb += lbatpos[t] <= signelbat[t] * pmax, 
+            pb += lbatneg[t] <= (1 - signelbat[t]) * pmax, 
+            pb += l[t] == lbat[t] - lPV[t], 
+            pb += a[t + 1] == a[t] + (self.env.battery.efficiency * lbatpos[t] - (1/self.env.battery.efficiency)*lbatneg[t])*dt, '''"charge_batterie_" + str(t)'''
+
+        
+
+        pb.setObjective(pulp.lpSum([conso_totale[t] * prix[t] * dt for t in range(T)])) #la fonction objectif pour minimiser le cout
+
+        pb.solve()
+        a = self.env.action_space.sample()
+        for t in range(T):
+            a[t]=l[t].varValue
+
+        return a
 
 if __name__ == "__main__":
     delta_t = datetime.timedelta(minutes=30)
